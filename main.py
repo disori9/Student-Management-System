4062c131ccc93dd65eb89a573eecaabb36d1a2f7
Ori <disori777@gmail.com>
2024-12-05 19:03:11 +0800

    Create a main window for the app that only has the Student Management System

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import sys
 
 
 class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+
+
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
